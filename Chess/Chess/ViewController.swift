e7d3a3724445b301f06e8bf552c171fc87c60e73
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-08-30 22:10:15 +0200

    Make scene renderable

--- Chess/Chess/ViewController.swift
+++ Chess/Chess/ViewController.swift
@@ -33,11 +33,11 @@ class ViewController: NSViewController {
         }
     }
     private func cameraAnimation() -> PNAnimatedCoordinateSpace {
-        let translation = PNIAnimatedValue<simd_float3>(keyFrames: [[0, -3, -10],
-                                                                    [0, -3, -10],
-                                                                    [0, -3, -10],
-                                                                    [0, -3, -10],
-                                                                    [0, -3, -10]],
+        let translation = PNIAnimatedValue<simd_float3>(keyFrames: [[0, 3, -10],
+                                                                    [0, 3, -10],
+                                                                    [0, 3, -10],
+                                                                    [0, 3, -10],
+                                                                    [0, 3, -10]],
                                                         times: [0, 2, 4, 6, 8],
                                                         maximumTime: 10)
         let orientation = PNIAnimatedValue<simd_quatf>(keyFrames: [simd_quatf(angle: Float(90).radians, axis: [0, 3.5, 0]),
@@ -57,7 +57,7 @@ class ViewController: NSViewController {
         let node = PNIAnimatedCameraNode(camera: camera,
                                          animator: PNIAnimator.default,
                                          animation: cameraAnimation())
-        let treeNode = PNNode(data: node, parent: scene.rootNode)
+        let treeNode = PNScenePiece.make(data: node, parent: scene.rootNode)
         scene.rootNode.children.append(treeNode)
         return node
     }
@@ -67,7 +67,7 @@ class ViewController: NSViewController {
                                  position: simd_float3) {
         let light = PNIAmbientLight(diameter: 40, color: color, intensity: intensity)
         let node = PNIAmbientLightNode(light: light, transform: .translation(vector: position))
-        let treeNode = PNNode(data: node, parent: scene.rootNode)
+        let treeNode = PNScenePiece.make(data: node, parent: scene.rootNode)
         scene.rootNode.children.append(treeNode)
     }
     private func addDirectionalLight(scene: PNScene,
@@ -84,7 +84,7 @@ class ViewController: NSViewController {
             return nil
         }
     }
-    private func loadObject(loader: PNSceneLoader, name: String) -> PNNode<PNSceneNode> {
+    private func loadObject(loader: PNSceneLoader, name: String) -> PNScenePiece {
         guard let model = loader.resource(name: name,
                                           extension: "obj",
                                           bundle: Bundle.main)?.rootNode else {
@@ -92,7 +92,7 @@ class ViewController: NSViewController {
         }
         return model
     }
-    private func set(material: PNMaterial, node: PNNode<PNSceneNode>) {
+    private func set(material: PNMaterial, node: PNScenePiece) {
         PNINodeInteractor().forEach(node: node) { n in
             guard let meshNode = n.data as? PNIMeshNode else {
                 return
@@ -102,12 +102,12 @@ class ViewController: NSViewController {
             }
         }
     }
-    private func load(loader: PNSceneLoader, name: String, material: PNMaterial) -> PNNode<PNSceneNode> {
+    private func load(loader: PNSceneLoader, name: String, material: PNMaterial) -> PNScenePiece {
         let object = loadObject(loader: loader, name: name)
         set(material: material, node: object)
         return object
     }
-    private func loadPieces(loader: PNSceneLoader, material: PNMaterial) -> [[PNNode<PNSceneNode>]] {
+    private func loadPieces(loader: PNSceneLoader, material: PNMaterial) -> [[PNScenePiece]] {
         let bishop = load(loader: loader, name: "Bishop", material: material)
         let knight = load(loader: loader, name: "Knight", material: material)
         let rook = load(loader: loader, name: "Rook", material: material)
@@ -115,13 +115,27 @@ class ViewController: NSViewController {
         let king = load(loader: loader, name: "King", material: material)
         let pawn = load(loader: loader, name: "Pawn", material: material)
         var nodes = [
-            [rook, knight, bishop, king, queen, bishop, knight, rook],
-            [pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn]
+            [load(loader: loader, name: "Rook", material: material),
+             load(loader: loader, name: "Knight", material: material),
+             load(loader: loader, name: "Bishop", material: material),
+             king,
+             queen,
+             load(loader: loader, name: "Bishop", material: material),
+             load(loader: loader, name: "Knight", material: material),
+             load(loader: loader, name: "Rook", material: material)],
+            [load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material),
+             load(loader: loader, name: "Pawn", material: material)]
         ]
         for j in 0 ..< 2 {
             for i in 0 ..< 8 {
-                let transform = PNNode(data: PNISceneNode(transform: .compose(translation: [-3.5 + Float(i), 2, Float(j)],
-                                                                              scale: [0.2, 0.2, 0.2])) as PNSceneNode)
+                let transform = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [-3.5 + Float(i), 2, Float(j)],
+                                                                                         scale: [0.2, 0.2, 0.2])))
                 transform.add(child: nodes[j][i])
                 nodes[j][i] = transform
             }
@@ -129,15 +143,16 @@ class ViewController: NSViewController {
         return nodes
     }
     private func loadBoardFields(loader: PNSceneLoader, mahogany: PNMaterial, sapele: PNMaterial) -> PNNode<PNSceneNode> {
-        let cubeMahogany = load(loader: loader, name: "Cube", material: mahogany)
-        let cubeSapele = load(loader: loader, name: "Cube", material: sapele)
-        let fields = PNNode(data: PNISceneNode(transform: .compose(translation: [-3.5, 2.5, 0],
-                                                                   scale: [1, 1, 1])) as PNSceneNode)
+        
+        let fields = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [-3.5, 2.5, 0],
+                                                                              scale: [1, 1, 1])))
         for i in 0 ..< 8 {
             for j in 0 ..< 8 {
-                let cubeNode = PNNode(data: PNISceneNode(transform: .compose(translation: [Float(i), -1.6, Float(j)],
-                                                                             scale: [0.5, 0.1, 0.5])) as PNSceneNode)
+                let cubeNode = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [Float(i), -1.6, Float(j)],
+                                                                                        scale: [0.5, 0.1, 0.5])))
                 let isWhite = ((i + j) % 2) == 0
+                let cubeMahogany = load(loader: loader, name: "Cube", material: mahogany)
+                let cubeSapele = load(loader: loader, name: "Cube", material: sapele)
                 cubeNode.add(child: isWhite ? cubeSapele : cubeMahogany)
                 fields.add(child: cubeNode)
             }
@@ -159,8 +174,8 @@ class ViewController: NSViewController {
     }
     private func loadBoard(loader: PNSceneLoader, material: PNMaterial) -> PNNode<PNSceneNode> {
         let board = load(loader: loader, name: "Board", material: material)
-        let boardTransform = PNNode(data: PNISceneNode(transform: .compose(translation: [0, 1.9, 3.5],
-                                                                           scale: [0.17, 0.2, 0.17])) as PNSceneNode)
+        let boardTransform = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [0, 1.9, 3.5],
+                                                                                      scale: [0.17, 0.2, 0.17])))
         boardTransform.add(child: board)
         return boardTransform
     }
@@ -181,14 +196,14 @@ class ViewController: NSViewController {
         cameraNode = addCamera(scene: engine.scene, position: [0, 2, -1])
         
         let board = loadBoard(loader: loader, material: sapeleMaterial)
-        let whites = PNNode(data: PNISceneNode(transform: .compose(translation: .zero,
-                                                                   scale: [1, 1, 1])) as PNSceneNode)
+        let whites = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: .zero,
+                                                                              scale: [1, 1, 1])))
         for row in loadPieces(loader: loader, material: sapeleMaterial) {
             whites.add(children: row)
         }
-        let black = PNNode(data: PNISceneNode(transform: .compose(translation: [0, 0, 7],
-                                                                  rotation: .init(angle: Float(180).radians, axis: [0, 1, 0]),
-                                                                  scale: .one)) as PNSceneNode)
+        let black = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [0, 0, 7],
+                                                                             rotation: .init(angle: Float(180).radians, axis: [0, 1, 0]),
+                                                                             scale: .one)))
         var mahoganyPieces = loadPieces(loader: loader, material: mahoganyMaterial)
         let translation = PNIAnimatedValue<simd_float3>(keyFrames: [[0, 0, 0],
                                                                     [0, 2, 0],
@@ -199,17 +214,17 @@ class ViewController: NSViewController {
         let space = PNAnimatedCoordinateSpace(translation: PNAnyAnimatedValue(translation),
                                               rotation: PNAnyAnimatedValue(PNIAnimatedValue.defaultOrientation),
                                               scale: PNAnyAnimatedValue(PNIAnimatedValue.defaultScale))
-        let node = PNNode<PNSceneNode>(data: PNIAnimatedNode(animator: PNIAnimator.default, animation: space))
+        let node = PNScenePiece.make(data: PNIAnimatedNode(animator: PNIAnimator.default, animation: space))
         node.add(child: mahoganyPieces[0][0])
         mahoganyPieces[0][0] = node
-        
+
         for row in mahoganyPieces {
             black.add(children: row)
         }
         
-        let all = PNNode(data: PNISceneNode(transform: .compose(translation: [0, 0, 0],
-                                                                rotation: .init(angle: Float(180).radians, axis: [0, 1, 0]),
-                                                                scale: [0.5, 0.5, 0.5])) as PNSceneNode)
+        let all = PNScenePiece.make(data: PNISceneNode(transform: .compose(translation: [0, 0, 0],
+                                                                           rotation: .init(angle: Float(180).radians, axis: [0, 1, 0]),
+                                                                           scale: [0.5, 0.5, 0.5])))
         let fields = loadBoardFields(loader: loader, mahogany: mahoganyMaterial, sapele: sapeleMaterial)
         all.add(children: whites, black, board, fields)
         engine.scene.rootNode.add(child: all)
@@ -223,5 +238,40 @@ class ViewController: NSViewController {
                             color: [1, 1, 1],
                             direction: simd_float3(0, -1, 0.1).normalized,
                             castsShadows: false)
+    }
+    override func mouseDown(with event: NSEvent) {
+        guard let frame = view.window?.frame, let cam = cameraNode else {
+            return
+        }
+        let location = event.locationInWindow
+        let point = PNPoint2D(Float(location.x/frame.width * 2 - 1),
+                              Float(location.y/frame.height * 2 - 1))
+        print(point)
+        
+        let nodes = engineView.interactor.pick(camera: cameraNode!,
+                                              scene: engine.scene,
+                                              point: point)
+        print(nodes)
+        nodes.forEach({
+            if let camera = $0.data as? PNCameraNode {
+            // nothing
+            } else {
+                _ = $0.removeSubtree()
+    //            if $0.children.isEmpty {
+    //                if let _ = $0.data as? PNMeshNode {
+    //                    _ = $0.removeSubtree()
+    //                }
+    //                if let _ = $0.data as? PNRiggedMesh {
+    //                    _ = $0.removeSubtree()
+    //                }
+    //                if let _ = $0.data as? PNAnimatedRiggedMesh {
+    //                    _ = $0.removeSubtree()
+    //                }
+    //                if let _ = $0.data as? PNAnimatedMesh {
+    //                    _ = $0.removeSubtree()
+    //                }
+    //            }
+            }
+        })
     }
 }
