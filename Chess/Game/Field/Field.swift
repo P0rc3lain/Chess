a4135e9606ded81d70ac587b1111e184147e901e
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-09-28 23:46:59 +0200

    Enhance assertions

--- Chess/Game/Field/Field.swift
+++ Chess/Game/Field/Field.swift
@@ -5,13 +5,13 @@
 //  Created by Mateusz Stompór on 11/09/2022.
 //
 
-import Foundation
+import Engine
 
 struct Field: Equatable {
     let row: Int
     let column: Int
     init(_ row: Int, _ column: Int) {
-        assert(row >= 0 && row < 8 && column >= 0 && column < 8)
+        assertAll(row >= 0, row < 8, column >= 0, column < 8)
         self.row = row
         self.column = column
     }
