b1eecb92976e8099c4e2a6b7d5db9c280076d59e
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-04-06 16:16:34 +0200

    Update target name, remove iOS target

--- ChessTests/Game/Move/Potential/PawnMoveGenerator+Tests.swift
+++ ChessTests/Game/Move/Potential/PawnMoveGenerator+Tests.swift
@@ -30,26 +30,4 @@ final class PawnMoveGeneratorTests: XCTestCase {
         XCTAssertTrue(actions[1].piecesToRemove.isEmpty)
         XCTAssertTrue(actions[1].sideEffects.isEmpty)
     }
-//    func testEnPassant() throws {
-//        var fields = Array(repeating: Array<Piece?>(repeating: nil, count: 8), count: 8)
-//        fields[7][4] = Piece(color: .white, type: .pawn(0))
-//        fields[6][6] = Piece(color: .black, type: .pawn(0))
-//        var current = Array(repeating: Array<Piece?>(repeating: nil, count: 8), count: 8)
-//        current[7][4] = Piece(color: .white, type: .pawn(0))
-//        current[6][4] = Piece(color: .black, type: .pawn(0))
-//        let previous = GameState(previous: nil,
-//                                 board: Board(fields: fields),
-//                                 selectedPiece: nil,
-//                                 turn: .white, expectation: ., checkState: <#T##CheckState#>)
-//        
-//        
-//        let actions = generator.potentialActions(piece: Piece(color: .white, type: .pawn(0)),
-//                                                 board: Board(fields: current),
-//                                                 previousBoard: Board(fields: fields))
-//        let action = actions.first(where: { $0.mainMove.to == Field(6, 5) })
-//        XCTAssertEqual(actions.count, 2)
-//        XCTAssertEqual(action?.mainMove.from, Field(7, 4))
-//        XCTAssertEqual(action?.mainMove.to, Field(6, 5))
-//        XCTAssertEqual(action?.piecesToRemove, [Piece(color: .black, type: .pawn(0))])
-//    }
 }
--- ChessTests/MovesGenerator+Tests.swift
+++ ChessTests/MovesGenerator+Tests.swift
@@ -9,24 +9,25 @@ import XCTest
 @testable import Chess
 
 final class MovesGeneratorTests: XCTestCase {
-//    let generator = MovesGenerator()
-//    let interactor = BoardInteractor()
+    let generator = MovesGenerator()
+    let interactor = BoardInteractor()
 
-//    func testCheck() throws {
-//        var fields = Array(repeating: Array<Piece?>(repeating: nil, count: 8), count: 8)
-//        fields[0][0] = Piece(color: .white, type: .king)
-//        fields[7][7] = Piece(color: .black, type: .king)
-//        fields[1][1] = Piece(color: .black, type: .pawn(0))
-//        let state = GameState(previous: nil,
-//                              board: Board(fields: fields),
-//                              selectedPiece: Piece(color: .black, type: .pawn(0)),
-//                              turn: .black,
-//                              expectation: .fieldPick)
-//        let whiteCheckingMoves = generator.checkingMoves(color: .white, state: state)
-//        let blackCheckingMoves = generator.checkingMoves(color: .black, state: state)
-//        XCTAssertEqual(blackCheckingMoves.count, 1)
-//        XCTAssertEqual(blackCheckingMoves[0].mainMove.from, Field(1, 1))
-//        XCTAssertEqual(blackCheckingMoves[0].mainMove.to, Field(0, 0))
-//        XCTAssertTrue(whiteCheckingMoves.isEmpty)
-//    }
+    func testCheck() throws {
+        var fields = Array(repeating: Array<Piece?>(repeating: nil, count: 8), count: 8)
+        fields[0][0] = Piece(color: .white, type: .king)
+        fields[7][7] = Piece(color: .black, type: .king)
+        fields[1][1] = Piece(color: .black, type: .pawn(0))
+        let state = GameState(previous: nil,
+                              board: Board(fields: fields),
+                              selectedPiece: Piece(color: .black, type: .pawn(0)),
+                              turn: .black,
+                              expectation: .field,
+                              checkState: .check)
+        let whiteCheckingMoves = generator.checkingMoves(color: .white, state: state)
+        let blackCheckingMoves = generator.checkingMoves(color: .black, state: state)
+        XCTAssertEqual(blackCheckingMoves.count, 4)
+        XCTAssertEqual(blackCheckingMoves[0].mainMove.from, Field(1, 1))
+        XCTAssertEqual(blackCheckingMoves[0].mainMove.to, Field(0, 0))
+        XCTAssertTrue(whiteCheckingMoves.isEmpty)
+    }
 }
