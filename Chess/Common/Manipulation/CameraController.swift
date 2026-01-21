5dd60a6f9ac4eed3e443118c411bb3d5d4deedd0
Mateusz Stompór <mateusz.stompor@gmail.com>
2026-01-21 22:30:27 +0100

    Fix camera displacement

--- Chess/Common/Manipulation/CameraController.swift
+++ Chess/Common/Manipulation/CameraController.swift
@@ -10,7 +10,7 @@ import simd
 
 class CameraController {
     func rotate(camera: PNAnimatedCameraNode, angleDegress: Int) {
-        let translation = PNAnimatedFloat3.static(from: [0, 0, 5])
+        let translation = PNAnimatedFloat3.static(from: [0, 0.41, 5])
         let transform = camera.animator.transform(coordinateSpace: camera.animation).rotation
         let newOrientation = simd_quatf(angle: Float(angleDegress).radians, axis: [0, 1, 0])
         let animation = PNKeyframeAnimation<simd_quatf>(keyFrames: [transform, newOrientation * transform],
